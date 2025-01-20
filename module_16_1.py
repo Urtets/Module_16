873efff6284a3a495ca361bcb1f84894cbc8b64a
Юрий <urtets@gmail.com>
2025-01-20 21:00:27 +0300

    Module_16 homework

--- module_16_1.py
+++ module_16_1.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-async def main_page():
+async def get_main_page():
     return "Главная страница"
 
 @app.get('/user/admin')
-async def admin_user():
+async def get_admin_page():
     return "Вы вошли, как администратор"
 
 @app.get('/user/{user_id}')
-async def id_user(user_id: int):
+async def get_user_id(user_id: int):
     return f'Вы вошли как пользователь №{user_id}'
 
 @app.get('/user')
-async def address_line(username: str, age: int):
+async def get_user_info(username: str, age: int):
     return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
