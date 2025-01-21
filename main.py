f9c0ee94d729ca6f651c32c1c163bb3d28b8d0bf
Юрий <urtets@gmail.com>
2025-01-21 12:48:19 +0300

    Module_16 homework

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -18,9 +19,16 @@ async def id_paginator(username: str = 'Uri', age: int = 24) -> dict:
     return {'user': username, 'Age': age}
 
 
-@app.get("/user/{first_name}/{last_name}") # В адресной строке вводитя /user/Имя/Фамилия
-async def news(first_name: str, last_name: str) -> dict:
-    return {'message': f'Hello, {first_name} {last_name}'}
+@app.get("/user/{username}/{id}") # В адресной строке вводитя /user/Имя/Фамилия
+async def news(username: Annotated[str, Path(min_length=3, max_length=15, description='Enter your username', example='Alex56')], # Annotated снимает обязательство вводить обязательные данные во второй параметр
+               id: int) -> dict:
+    return {'message': f'Hello, {username} {id}'}
+
+
+# @app.get("/user/{username}/{id}") # В адресной строке вводитя /user/Имя/Фамилия
+# async def news(username: str = Path(min_length=3, max_length=15, description='Enter your username', example='Alex56'),
+#                id: int = Path(ge=0, le=100, description='Enter your id', example='75')) -> dict:
+#     return {'message': f'Hello, {username} {id}'}
 
 
 
